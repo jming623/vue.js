e35d8fb8cff79a4fdff81ab89354e624842541c6
jming623 <jming623@naver.com>
2022-06-04 23:55:28 +0900

    vue.js 3일차

---
+++ test3/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './routes';
+
+Vue.config.productionTip = true;
+
+createApp({
+    render: h => h(App)
+    ,router
+}).mount('#app')
